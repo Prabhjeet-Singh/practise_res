0370fcfdb21756c56e23817242befa96fbab68ca
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-04-26 22:59:00 -0400

    adding reset btn

--- redux_1/src/features/counter/Counter.js
+++ redux_1/src/features/counter/Counter.js
@@ -1,10 +1,19 @@
 import React from "react";
 import {useSelector ,useDispatch } from "react-redux";
-import {increment, decrement } from "./counterSlice"
+import {increment, decrement, reset, incrementByAmount } from "./counterSlice"
+import {useState} from "react";
 
 const Counter = () => {
     const count=useSelector((state)=>state.counter.count)
     const dispatch =useDispatch();
+
+    const [incrementAmount, setIncrementAmount]=useState(0)
+    const addValue=Number(incrementAmount)||0;
+
+    const resetAll=()=>{
+      setIncrementAmount(0);
+      dispatch(reset());
+    }
   return (
     <section>
         <p>{count}</p>
@@ -12,6 +21,12 @@ const Counter = () => {
             <button onClick={()=>dispatch(increment())}>+</button>
             <button onClick={()=>dispatch(decrement())}>-</button>
         </div>
+        <input type="text" name="" value={incrementAmount} onChange={(e)=>setIncrementAmount(e.target.value)}/>
+
+          <div>
+            <button type="" onClick={()=>dispatch(incrementByAmount(addValue))}>Add Amount</button>
+            <button type="" onClick={resetAll}>reset</button>
+          </div>
     </section>
   );
 };
