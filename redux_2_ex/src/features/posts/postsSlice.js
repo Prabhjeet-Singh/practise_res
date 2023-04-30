687b32c7682bb931aab43cf43f95247cca6e4848
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-04-29 21:46:01 -0400

    adding input field and liquid state

--- redux_2_ex/src/features/posts/postsSlice.js
+++ redux_2_ex/src/features/posts/postsSlice.js
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit'
+import {createSlice,nanoid} from '@reduxjs/toolkit'
 
 const initialState=[
     {id:'1',title:'Learning redux toolkit',content:"I've heard good thing."},
@@ -9,8 +9,23 @@ const postsSlice= createSlice({
     name:'posts',
     initialState,
     reducers:{
-
+        postAdded:{
+            reducer(state,action){
+        state.push(action.payload)
+    },
+    prepare(title,content){
+        return{
+            payload:{
+                id:nanoid(),
+                title, 
+                content
+            }
+        }
     }
+    }
+  }
 })
+export const selectAllPosts =(state)=>state.posts
+export const {postAdded} =postsSlice.actions
 
 export default postsSlice.reducer
