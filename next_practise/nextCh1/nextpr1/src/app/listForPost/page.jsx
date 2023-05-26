f3299d1c891f9692733b414ca8d7d0d6fac8977a
Prabhjeet Singh <prabhijeetsidhu@gmail.com>
2023-05-25 22:16:00 -0400

    -up update some function

--- next_practise/nextCh1/nextpr1/src/app/listForPost/page.jsx
+++ next_practise/nextCh1/nextpr1/src/app/listForPost/page.jsx
@@ -1,15 +1,25 @@
 const getPostData=async ()=>{
     const res= await fetch("https://jsonplaceholder.typicode.com/posts");
-    return res.json;
+    return res.json();
+}
+const getUsers= async ()=>{
+    const res= await fetch("https://jsonplaceholder.typicode.com/users");
+    return res.json();
+
 }
 
 export default async function listForPost(){
-    const data= await getPostData();
+    const [data,usersData]= await Promise.all([getPostData(),getUsers()]);
     return(
         <>
         {data.map((data)=>{
-            return (<span key={data.id}>{data.title}</span>)
-            })}
+            return (<p key={data.id}>{data.title}</p>)
+            })
+        }
+        {usersData.map((data)=>{
+            return (<p key={data.id}>{data.name}</p>)
+            })
+        }
         </>
     )
 }
